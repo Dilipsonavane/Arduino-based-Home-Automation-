1ddc5af87b27b2cf490f9541dcfb9c1f4ad78571
Dilipsonavane <dipusonavane@gmail.com>
2020-03-19 11:24:12 +0530

    Add files via upload

---
+++ Home Automation.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(7,INPUT);
+  pinMode(6,OUTPUT);
+  pinMode(8,OUTPUT);
+  digitalWrite(6,HIGH);
+  digitalWrite(8,HIGH);
+  
+}
+
+void loop() {
+  if(!digitalRead(7))
+    digitalWrite(8,HIGH);
+  else
+    digitalWrite(8,LOW);
+}
